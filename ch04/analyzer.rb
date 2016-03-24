ef8694c4f8d2f5db2830a47c60dfe55b7e594c04
rb <rb@email.com>
2016-03-23 17:14:37 -0700

    Req: did the word count.

--- ch04/analyzer.rb
+++ ch04/analyzer.rb
@@ -15,4 +15,9 @@ puts "#{total_characters} characters"
 #Req 2 - Character count (excluding spaces)
 total_characters_nospaces = text.gsub(/\s+/, '').length #remove all series of 1 or more spaces in text.
 puts "#{total_characters_nospaces} characters(excluding spaces)"
- 
+
+#Req 4 - Word count
+# Use split instead of scan. Why ? Scan will consider words like First-class as two words instead of one.
+word_count = text.split.length # By default, split will split on space char. So, no need to specify
+puts "#{word_count} words"
+
