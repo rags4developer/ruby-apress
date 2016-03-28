cf305e7d9980b13fbc1b3bf2850509bf1b95ce18
rb <rb@email.com>
2016-03-28 16:51:22 -0700

    TDD-Added the example. Developing code step by step, using TDD.

---
+++ ch08/test_titelize.rb
@@ -0,0 +1,28 @@
+
+=begin
+Ruby allows us to add a method to existing Ruby classes or even redefine existing methods. This is called "monkey patching", i.e. re-opening any class and changing it's methods. 
+
+We want to add a method "titelize" to String class which will capitalize the first alphbet of each
+word in a string. Lets develop this code with the TDD method, using Test::Unit. 
+
+
+
+=end
+
+class String
+	def titelize
+		
+	end
+end
+
+
+# First, write all the tests that you want this class method to pass.
+require 'test/unit'
+
+class TestTitelize < Test::Unit::TestCase
+	def test_basic
+		assert_equal("This is a test", "this is a test".titelize)
+		assert_equal("Another Test 1234", "another test 1234".titelize)
+		assert_equal("We're Testing", "We're testing".titelize)
+	end
+end
